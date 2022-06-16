7e4ca615c4927c419568fe3a4bee2014707c219d
AICO <aicodeen@gmail.com>
2022-06-16 17:52:02 +0100

    latest commits

---
+++ ak.js
@@ -0,0 +1,10 @@
+ //Get access to the button
+ const button = document.querySelector('button');
+
+ //Add event listener to the button
+ button.addEventListener('click', welcomeArkardia);
+
+ //Function to show my name
+ function welcomeArkardia() {
+     alert('Welcome to Arkardia');
+ }
